f29a48e259eb956da69f2cfd3358ccf1cd75db39
Syrrel Cabiling <syrrelcabiling@syrrels-mbp.fc.ca>
2018-03-08 11:31:05 -0500

    added final files that I have been working on

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 	console.log('hangman script fired!');
 
 	//create an array to hold the words to be gueesed (MDN ->arrays, MDN const)
-	const words = ["dog","cat","bird","horse","snake"];
+	const words = ["bird","cat","dog","horse","snake"];
 
 	//set up variable stack
 	//css selector
@@ -12,35 +12,41 @@
 		wordHint = document.querySelector('.current-word'),
 		guessBox = document.querySelector('input'),
 		wrongGuesses = 0,
-		correctGuesses = 0,
+    correctGuesses = 0,
 		resetScreen = document.querySelector('.reset-screen'),
 		resetButton = resetScreen.querySelector('button'),
 		wrongLetterList = document.querySelector('.wrong-letters'),
 		wrongLetterArray = [];
 
-	function showResetScreen(message) {
-			//user has lost, reset stuff and start over
-			resetScreen.classList.add('show-piece');
-			//grabs the header tag and change its content ==> replace it with the message
-			resetScreen.querySelector('h1').textContent = message;
-	}
 
 
-	//the first place to go when youre lost and a word says undefined is here
+function showResetScreen(message) {
+resetScreen.classList.add('show-piece');
+//grab header tag and change uts content => replace it with the message
+resetScreen.querySelector('h3').textContent = message;
+}
+
 
 	function resetGame() {
-		//debugger;
+	//	debugger;
 
 		let gamePieces = Array.from(document.querySelectorAll('.show-piece'));
 		gamePieces.forEach(piece => piece.classList.remove('show-piece'));
 		wrongGuesses = 0;
 		guessBox.value = "";
 		wrongLetterList.textContent = "";
-		wrongLetterArray.textContent = "";
 		wrongLetterArray = [];
+
 	}
 
 	init();
+
+
+	function showResetScreen() {
+		//user has lost, reset stuff and start over
+		console.log('you lose, looser!');
+		resetScreen.classList.add('show-piece');
+}
 
 	function takeGuess() {
 		//debugger;
@@ -48,57 +54,51 @@
 		console.log(this.value);
 
 		//if there is no value then exit the game loop -> MDN 'or'
-		if (this.value =="" || this.value.length < 1 ) {
+		if (this.value =="" ||this.value.length < 1 ) {
 			return;
 		}
 
-		let currentGuess = this.value; //this is the current letter in the input
+    let currentGuess = this.value; //this is the current letter in the input
 
 
-	//if (currentWord.indexOf(this.value) < 0) {}
 	//set up the win loose paths (if/else)
 	if (!currentWord.includes(this.value)) {
 		//loosing path
 		console.log('valid letter!');
 		wrongLetterArray.push(this.value);
-		wrongLetterList.textContent = wrongLetterArray
-		wrongLetters.textContent = wrongLetterArray.join(" ");
+		wrongLetterList.textContent = wrongLetterArray.join(" ");
 		document.querySelector(`.wrong${wrongGuesses}`).classList.add('show-piece');
 
 		if (wrongGuesses >= 5) {
-		//console.log('you lose, loser!')
 
 		//add incriment values
-		showResetScreen("GAME OVER. You ran out of guesses!");
+		showResetScreen();
 		} else {
 			//if you loose, rest the screen
 			wrongGuesses++;
-			//its a number i made it, and it will be the last step
 		}
 
 		//compare the letter guess to the word to see if it's in there
 	} else {
-		//debugger;
-		let matchAgainst = currentWord.split("");
-		var hintString = wordHint.textContent.split(" ");
-
-		matchAgainst.forEach((letter, index) => {
-			if (letter === currentGuess) {
-				hintString[index] = currentGuess;
-				correctGuesses++; //makes sure to track correct guesses
-			} //a function is just like a css rule, just a different structure, to the left is an object on the page, and everything that comes after the . is an array aka like a command, a part of the Javascript library
-			//
-		});
-
-		wordHint.textContent = ""; //make the hint on the screen be empty
-		wordHint.textContent = hintString.join(" ");
-
-		if (correctGuesses === currentWord.length) {
-			showResetScreen();
-		}
-		//paragraph(the thing youre working with) aka WORDHINT then the thing you want to change aka TEXTCONTENT
 		//winning path
-		}
+    let matchAgainst = currentWord.split(" ");
+    let hintString = wordHint.textContent.split(" ");
+
+    matchAgainst.forEach((letter, index) => {
+      if (letter === currentGuess) {
+        hintString[index] = currentGuess;
+        correctGuesses++; //make sure to track correct guesses
+      }
+    });
+
+    wordHint.textContent = ""; //make hint on the screen be empty
+    wordHint.textContent = hintString.join(" ");
+
+    if (correctGuesses === currentWord.length) {
+      	showResetScreen("You Won!");
+    }
+
+	 }
 	}
 
 	function init() {
@@ -111,7 +111,7 @@
 		console.log(`guess this word: ${currentWord}. it's at ${words.indexOf(currentWord)}`);
 
 	}
-	//debugger;
+		//debugger;
 	//console.log(words[3]);
 
 
